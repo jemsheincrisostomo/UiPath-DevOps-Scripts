bd4f9820d7b2c332576b8d6717eaf77915353133
jemsheincrisostomo <61526253+jemsheincrisostomo@users.noreply.github.com>
2023-09-08 15:37:30 +0800

    Add files via upload
    updated script

--- scripts/UiPathRunTest.ps1
+++ scripts/UiPathRunTest.ps1
@@ -318,7 +318,7 @@ if($secretIndex -ge 0){
 WriteLog "Executing $uipathCLI $ParamMask"
 
 #call uipath cli 
-& "$uipathCLI" $ParamList.ToArray()
+& "$uipathCLI" "$ParamList.ToArray()"
 #Invoke-Expression "& `"$uipathCLI`" $ParamList.ToArray()";
 #[System.Diagnostics.Process]::Start("$uipathCLI", "$ParamList.ToArray()")
 #Start-Process -NoNewWindow -FilePath "$uipathCLI" -ArgumentList "$ParamList.ToArray"
